ed9e52a4e00682d02b505a1621be59ea12e3a62d
codecademy <ccuser@codecademy.com>
2020-07-24 20:57:43 +0000

    destination index complete

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
+#background information
 destinations = ["Paris,France", "Shanghai,China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+#getting destination index
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+print(get_destination_index("Paris,France"))
+
