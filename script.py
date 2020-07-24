9fd7daefade27efaf1145b7903c6df3cf81861a1
codecademy <ccuser@codecademy.com>
2020-07-24 23:27:34 +0000

    Created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -16,3 +16,31 @@ def get_traveler_location(traveler):
   return traveler_destination_index
 test_destination_index = get_traveler_location(test_traveler)
 print(test_destination_index)
+
+#list of attractions
+attractions = [[] for destination in destinations]
+
+def add_attraction(destination, attraction):
+ try:
+  destination_index = get_destination_index(destination)
+  attractions_for_destination = attractions[destination_index]
+  attractions_for_destination.append(attraction)
+  return attractions_for_destination
+ except ValueError:
+   return
+
+add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("Sao Paulo, Brazil", ["Sao Paulo Zoo", ["zoo"]])
+add_attraction("Sao Paulo, Brazil", ["Patio do Colegio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+print(attractions)
+
+
+
